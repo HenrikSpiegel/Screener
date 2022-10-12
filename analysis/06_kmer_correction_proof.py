2f8034abdd3eec8d55276b5f3304d7a8caccfc4e
Henrik Sparre Spiegelhauer <henspi@g-12-l0002.cm.cluster>
2022-10-12 10:19:15 +0200

    Updated 06_ to also run multiple read lengths in addition to multiple error rates.

--- analysis/06_kmer_correction_proof.py
+++ analysis/06_kmer_correction_proof.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import random
 import sys
 import statsmodels.api as sm
@@ -9,16 +7,17 @@ import plotly.express as px
 import numpy as np
 from pathlib import Path
 
+import plotly.io as pio
+pio.kaleido.scope.mathjax = None
 #
-output_dir = Path("Results/") / Path(__file__).stem
+output_dir = Path("results/") / Path(__file__).stem
 
 # simulation controls:
 experiments = 10000
 error_rates = np.array([0,1,2,3,4,5])
+read_lenghts = np.array([50,100,150,200,300])
 k = 21
 
-L = 150
-sequence_L = 3*L
 catalogue_size = 20
 
 def kmerize(seq, k):
@@ -31,9 +30,13 @@ substituion_dict = {b:[*(bases_set).difference(set(b))] for  b in bases_set}
 def substitute(base):
     return random.choice(substituion_dict[base])
 
-def main():
+def error_rate_variations():
     # run simmulations
-    print("Running simulations", file=sys.stderr)
+    print("Running error rate simulations", file=sys.stderr)
+
+    L = 150
+    sequence_L = 3*L
+
     count = np.zeros((len(error_rates), experiments))
     for j in trange(len(error_rates)):
         err_rate = error_rates[j]
@@ -72,15 +75,68 @@ def main():
     output_dir.mkdir(parents=True, exist_ok=True)
 
     df_total = pd.concat([df_count, df_count_err, df_count_final])
-    df_total.to_csv(output_dir/"counts.csv", index=False)
+    df_total.to_csv(output_dir/"counts_err.csv", index=False)
 
     df_total_long = df_total.melt(id_vars="type", var_name="Error Rate", value_name="Coverage Estimate")
     fig = px.box(df_total_long, x="type", y="Coverage Estimate", color="Error Rate",
-            title="Coverage estimates from simulated read of various sequencing errors<br><sup>Simulated coverage is 33%",
+            title="Coverage estimates from simulated read of various sequencing errors<br><sup>Simulated coverage is 33% with read length 150",
             labels={"type":""})
     fig.add_hline(y=1/3, line_dash='dot', annotation_text="Coverage", annotation_position="top left")
-    fig.write_image(output_dir/"coverage_estimates.png", width=1000, height=500)
+    fig.write_image(output_dir/"coverage_estimates_error.png", width=1000, height=500)
+
+def read_length_variations():
+    # run simmulations
+    print("Running read length simulations", file=sys.stderr)
+    err_rate = 3
+    count = np.zeros((len(read_lenghts), experiments))
+    for j in trange(len(read_lenghts)):
+        L = read_lenghts[j]
+        sequence_L = 3*L
+        for i in trange(experiments, leave=False):
+            true_sequence = np.array([random.choice(bases) for i in range(sequence_L)])
+            true_read     = true_sequence[0:L]
+            catalogue = set(random.choice(kmerize(true_sequence, k)) for i in range(catalogue_size))
+
+            read = true_read.copy()
+            is_error = np.random.randint(1,100, L) <= err_rate
+            read[is_error] = [substitute(x) for x in read[is_error]]
+
+            kmers_obs = list(kmerize(read, k))
+            count[j, i] = sum(x in catalogue for x in kmers_obs) / len(catalogue) #TODO: Use a distribution based center metric (NB center)
+
+    print("Adding error corrections and plotting", file=sys.stderr)
+    # Add error correction
+    error_rate_correction = (1-err_rate/100)**k
+    count_error_corrected = count / error_rate_correction
+
+    edge_loss_correction = 1 - ((k-1)/read_lenghts)
+    count_fully_corrected = count_error_corrected / edge_loss_correction[:,np.newaxis]
+
+    cols = [f"Read Length: {x}bp" for x in read_lenghts]
+    df_count = pd.DataFrame(count.T, columns = cols)
+    df_count["type"] = "count"
+
+    df_count_err = pd.DataFrame(count_error_corrected.T, columns = cols)
+    df_count_err["type"] = "Error corrected count"
+
+    df_count_final = pd.DataFrame(count_fully_corrected.T, columns = cols)
+    df_count_final["type"] = "Error and Edge loss corrected count"
+
+    print(f"Writing results to -> {output_dir}")
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    df_total = pd.concat([df_count, df_count_err, df_count_final])
+    df_total.to_csv(output_dir/"counts_readlength.csv", index=False)
+
+    df_total_long = df_total.melt(id_vars="type", var_name="Read Length", value_name="Coverage Estimate")
+    fig = px.box(df_total_long, x="type", y="Coverage Estimate", color="Read Length",
+            title="Coverage estimates from simulated read of various read lengths<br><sup>Simulated coverage is 33% at 3% sequencing error",
+            labels={"type":""})
+    fig.add_hline(y=1/3, line_dash='dot', annotation_text="Coverage", annotation_position="top left")
+    fig.write_image(output_dir/"coverage_estimates_readlength.png", width=1000, height=500)
 
 if __name__ == "__main__":
     print("Running" + __file__, file=sys.stderr)
-    main()
+    error_rate_variations()
+    read_length_variations()
+    print("Finished: "+__file__, file=sys.stderr)
--- scripts/add_to_que.py
+++ scripts/add_to_que.py
@@ -1,6 +1,6 @@
 import argparse
 from functions import submit2
-
+from pathlib import Path
 
 
 default_args = dict(
@@ -9,6 +9,8 @@ default_args = dict(
         cores = 10,
         ram=40,
         group="dtu_00009",
+
+
     )
 
 
@@ -16,16 +18,20 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--command", required=True, help="path to script")
-    parser.add_argument("--name", default="add_to_que_default", help="jobname")
+    parser.add_argument("--name", default="addQue_def", help="jobname")
     parser.add_argument("--test", action="store_true", help="Prints the qsub job-script to stdout and doesn't add_to_que")
     parser.add_argument("-o", nargs="+", help="options in kw:arg kwargs passed to submit2")
     args = parser.parse_args()
 
-
+    log_dir = Path("logs/add_to_que")
+    log_dir.mkdir(parents=True, exist_ok=True)
+    
     final_args = default_args.copy()
     user_args = {
         "jobname": args.name,
-        'command': args.command
+        'command': args.command,
+        'error': (log_dir/(args.name+".stderr")).as_posix(),
+        'output':(log_dir/(args.name+'.stdout')).as_posix()
     }
     
     if args.o:
